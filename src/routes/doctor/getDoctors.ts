e40b8f6577441174addc4fbc5fe6543ee621401c
Ullyanne <ujfp@ic.ufal.br>
2024-05-14 15:51:24 -0300

    ✨ create get doctors route

---
+++ src/routes/doctor/getDoctors.ts
@@ -0,0 +1,74 @@
+import { ZodTypeProvider } from 'fastify-type-provider-zod';
+import { FastifyInstance } from "fastify";
+import { z } from "zod"
+import { prisma } from '@lib/prisma';
+
+export async function getDoctors(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .get('/doctors', {
+      schema: {
+        querystring: z.object({
+          search: z.string().optional(),
+          pageIndex: z.string().optional().nullable().default('0').transform(Number)
+        }),
+        response: {
+          200: z.object({
+            doctors: z.array(
+              z.object({
+                crm: z.string(),
+                name: z.string(),
+                specialty: z.string(),
+              })
+            ),
+            doctorsAmount: z.number(),
+          })
+        }
+      }
+    }, async (request, reply) => {
+
+      const searchQuery = request.query.search
+      const filteredSearchQuery = typeof searchQuery === 'string'
+        ? searchQuery.replace(/[^\p{L}\p{N}\s%]/gu, '').replace(/[\s]/, '&')
+        : '';
+
+      const pageIndex = request.query.pageIndex
+
+      const doctorsAmount = await prisma.doctor.count()
+
+      const doctors = await prisma.doctor.findMany({
+        select: {
+          crm: true,
+          name: true,
+          specialty: true
+        },
+        where: {
+          OR: [
+            {
+              crm: {
+                startsWith: filteredSearchQuery
+              }
+            },
+            {
+              name: {
+                search: filteredSearchQuery
+              }
+            },
+            {
+              specialty: {
+                search: filteredSearchQuery
+              }
+            },
+          ]
+        },
+
+        take: 10,
+        skip: pageIndex * 10,
+        orderBy: {
+          createdAt: 'desc'
+        }
+      })
+
+      return reply.send({ doctors: doctors, doctorsAmount: doctorsAmount })
+    })
+}
