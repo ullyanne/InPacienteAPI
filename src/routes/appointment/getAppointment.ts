7d3d7a530527caa8eae3aa2e996ba69abd8e3b2f
Ullyanne <ujfp@ic.ufal.br>
2024-05-14 15:50:08 -0300

    ✨ create get appointment route

---
+++ src/routes/appointment/getAppointment.ts
@@ -0,0 +1,50 @@
+import { ZodTypeProvider } from 'fastify-type-provider-zod';
+import { FastifyInstance } from "fastify";
+import { z } from "zod"
+import { prisma } from '@lib/prisma';
+
+export async function getAppointment(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .get('/appointments/:appointmentId', {
+      schema: {
+        params: z.object({
+          appointmentId: z.string()
+        }),
+        response: {
+          200: z.object({
+            appointment: z.object({
+              id: z.string().uuid(),
+              date: z.date(),
+              doctorCrm: z.string(),
+              patientCpf: z.string()
+            })
+          })
+        }
+      }
+    }, async (request, reply) => {
+
+      const { appointmentId } = request.params
+
+      const appointment = await prisma.appointment.findUnique({
+        select: {
+          id: true,
+          date: true,
+          doctorCrm: true,
+          patientCpf: true
+        },
+        where: {
+          id: appointmentId,
+        }
+      })
+
+      if (appointment === null) {
+        throw new Error('Patient not found.')
+      }
+
+      return reply.send(
+        {
+          appointment: appointment
+        })
+    })
+}
