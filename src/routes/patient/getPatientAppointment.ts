6446028c9b4c471d73c250e9b0ef3215eb6bc379
Ullyanne <ujfp@ic.ufal.br>
2024-05-14 16:07:56 -0300

    🚚 move get patient appointments to patients routes folder

---
+++ src/routes/patient/getPatientAppointment.ts
@@ -0,0 +1,57 @@
+import { ZodTypeProvider } from 'fastify-type-provider-zod';
+import { FastifyInstance } from "fastify";
+import { z } from "zod"
+import { prisma } from '@lib/prisma';
+
+export async function getPatientAppointment(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .get('/patients/:patientCpf/appointments', {
+      schema: {
+        params: z.object({
+          patientCpf: z.string()
+        }),
+        response: {
+          200: z.object({
+            patientAppointments: z.array(
+              z.object({
+                id: z.string().uuid(),
+                date: z.date(),
+                doctor: z.object({
+                  name: z.string(),
+                  specialty: z.string()
+                }),
+              })
+            )
+          })
+        }
+      }
+    }, async (request, reply) => {
+
+      const { patientCpf } = request.params
+
+      const patientAppointments = await prisma.appointment.findMany(
+        {
+          select: {
+            id: true,
+            date: true,
+            doctor: {
+              select: {
+                name: true,
+                specialty: true
+              }
+            },
+          },
+
+          where: {
+            patientCpf: patientCpf,
+            date: {
+              gte: new Date()
+            }
+          },
+        }
+      )
+
+      return reply.status(200).send({ patientAppointments: patientAppointments })
+    })
+}
